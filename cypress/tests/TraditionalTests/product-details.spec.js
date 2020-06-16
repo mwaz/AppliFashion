266050dc816c88f7484f1a693cef8766f2f76386
Waweru <amby.mwaz@gmail.com>
2020-06-17 01:59:26 +0300

    fix: rename prod-experience file

---
+++ cypress/tests/TraditionalTests/product-details.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+import {
+  viewPortSize,
+  checkSizes,
+  switchViewports,
+  fetchTestTitle,
+  shouldBeVisible
+} from '../../utils/utilityFunctions';
+import { actions, elements, expects } from '../../pages/pageObjects';
+
+const viewports = {
+  laptop: [1200, 700],
+  tablet: [768, 700],
+  mobile: [375, 812],
+}; 
+const { laptop, tablet, mobile } = viewports;
+const testTask = 3;
+
+context('Product Experience', () => {
+  beforeEach(() => {
+      cy.visit('https://demo.applitools.com/gridHackathonV1.html')
+  });
+
+  describe('All Viewport tests', () => {
+      const viewports = [tablet, mobile, laptop];
+      viewports.forEach((size) => {
+          
+          describe(`Filter for black shoes [${checkSizes(size)}]`, () => {
+              it(`can click the first shoe image and validate details`, function(){
+                switchViewports(size)
+                  actions.clickFirstBlackShoe();
+                  expects.validateProductImageElements()
+                  expects.validateProductImageUrl({productId : 1});
+                  shouldBeVisible(fetchTestTitle(this), elements.productPageShoeImage(), viewPortSize(size), testTask);
+              });
+             
+          })
+      });
+  });
+});
