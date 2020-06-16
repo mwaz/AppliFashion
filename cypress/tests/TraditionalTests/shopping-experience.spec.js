73f98f88e6f10ee068f869d4dfa97436576f42c0
Waweru <amby.mwaz@gmail.com>
2020-06-17 02:00:06 +0300

    fix: update test naming reference

--- cypress/tests/TraditionalTests/shopping-experience.spec.js
+++ cypress/tests/TraditionalTests/shopping-experience.spec.js
@@ -1,75 +1,75 @@
 /// <reference types="cypress" />
 import {
-    shouldBeVisible,
     shouldEqual,
-    shouldInvokeAttribute,
     viewPortSize,
     checkSizes,
-} from '../../utils/utilityFunctions';
-import { actions, elements, expects } from '../../pages/pageObjects';
-
-context('Filter Results', () => {
+    switchViewports,
+    fetchTestTitle
+  } from '../../utils/utilityFunctions';
+  import { actions, elements, expects } from '../../pages/pageObjects';
+  
+  const viewports = {
+    laptop: [1200, 700],
+    tablet: [768, 700],
+    mobile: [375, 812],
+  }; 
+  const { laptop, tablet, mobile } = viewports;
+  const testTask = 2;
+  
+  context('Product Experience', () => {
     beforeEach(() => {
         cy.visit('https://demo.applitools.com/gridHackathonV1.html')
     });
     afterEach(() => {
         actions.resetFilter();
     });
-
+  
     describe('Desktop Tests', () => {
-        const desktopViewport = [1200, 700];
-
+  
         beforeEach(() => {
-            cy.viewport(desktopViewport[0], desktopViewport[1])
+            cy.viewport(laptop[0], laptop[1])
         });
-        describe('Filter for black shoes (1200 X 700)', () => {
-            const testName = "sampleTestname"
+        describe('Filter for black shoes on viewport (1200 X 700)', () => {
             it('can filter all black shoes based on color', function(){
                 expects.colorFilterIsVisible();
                 actions.clickBlackColorFilter();
                 actions.clickFilterButton();
-                shouldEqual(1, this.test.title, elements.gridItems(), viewPortSize(desktopViewport), 2);
+                shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(laptop), 2, testTask);
             });
-            it('can filter the first black shoe based on the price', () => {
+            it('can filter the first black shoe based on the price', function(){
                 expects.colorFilterIsVisible();
                 actions.clickZeroToFiftyPriceFilter();
                 actions.clickFilterButton();
-                shouldEqual(2, "shows single filtered black shoes based on price", elements.gridItems(), viewPortSize(desktopViewport), 1);
-
+                shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(laptop), 1, testTask);
+  
             });
         });
     });
-    describe('Tablet and Iphone-X (763 X 700, 375 X 812)', () => {
-        const viewports = [[763, 700], [375, 812]];
+    describe('Tablet and Iphone-X (768 X 700, 375 X 812)', () => {
+        const viewports = [tablet, mobile];
         viewports.forEach((size) => {
-            beforeEach(() => {
-                if (Cypress._.isArray(size)) {
-                    cy.viewport(size[0], size[1])
-
-                } else {
-                    cy.viewport(size)
-                }
-            });
             afterEach(() => {
                 actions.resetFilter();
             })
             describe(`Filter for black shoes [${checkSizes(size)}]`, () => {
-                it(`can filter all black shoes based on color [${checkSizes(size)}]`, () => {
+                it(`can filter all black shoes based on color`, function(){
+                  switchViewports(size)
                     actions.openFilterNavigationSideBar();
                     expects.colorFilterIsVisible();
                     actions.clickBlackColorFilter();
                     actions.clickFilterButton();
-                    shouldEqual(3, "shows  two filtered black shoes based on color", elements.gridItems(), viewPortSize(size), 2);
+                    shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(size), 2, testTask);
                 });
-                it(`can filter the first black shoe based on the price [${checkSizes(size)}]`, () => {
+                it(`can filter the first black shoe based on the price`, function(){
+                  switchViewports(size)
                     actions.openFilterNavigationSideBar();
                     expects.priceFilterIsVisible();
                     actions.clickZeroToFiftyPriceFilter();
                     actions.clickFilterButton();
-                    shouldEqual(4, "shows single filtered black shoes based on price", elements.gridItems(), viewPortSize(size), 1);
-
+                    shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(size), 1, testTask);
+  
                 });
             })
         });
     });
-});
+  });
