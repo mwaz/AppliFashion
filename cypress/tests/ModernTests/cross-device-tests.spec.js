452eb3cdd820157118415b585bb54ed027c094b7
Waweru <amby.mwaz@gmail.com>
2020-06-23 02:04:05 +0300

    fix: modern approach cross device tests

--- cypress/tests/ModernTests/cross-device-tests.spec.js
+++ cypress/tests/ModernTests/cross-device-tests.spec.js
@@ -1,33 +1,114 @@
+/// <reference types="@applitools/eyes-cypress" />
 import { actions, elements, expects } from '../../pages/pageObjects';
+import {
+    checkEyesWindow,
+    switchViewports,
+} from '../../utils/utilityFunctions';
+import {
+    viewportDimensions,
+    iphoneViewport,
+    tabletViewports,
+    desktopViewports
+} from '../../utils/viewports';
 
-describe("Modern Approach Tests", () => {
+const { tablet, mobile } = viewportDimensions;
 
-    beforeEach( () => {
+context('Cross-Device Elements Test', () => {
+
+    beforeEach(() => {
         actions.goToV1AppUrl();
-    })
-    it(`Task 1`, function () {
-        cy.eyesOpen({
-            testName: 'Task 1',
-        })
-
-        // check the login page with fluent api, see more info here
-        // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
-        cy.eyesCheckWindow({
-            tag: "Login Window",
-            target: 'window',
-            fully: true
-        });
-
-        cy.get('#log-in').click();
-
-        // Check the app page
-        cy.eyesCheckWindow({
-            tag: "App Window",
-            target: 'window',
-            fully: true
-        });
-
-        cy.eyesClose()
     });
+
+    describe('Task1: All Viewport Verification Tests', () => {
+        beforeEach(() => {
+            cy.eyesOpen({});
+        })
+        afterEach(() => {
+            cy.eyesClose();
+        });
+        describe(`Viewport verification tests`, function () {
+
+            it(`Task1: Main Header`, function () {
+                checkEyesWindow(elements.mainHeader());
+            });
+            it(`Task1: Main Navigation`, function () {
+                checkEyesWindow(elements.mainNavigation());
+            });
+
+            it(`Task1: Shoes Section`, function () {
+                checkEyesWindow(elements.shoesDisplaySection());
+            });
+            it(`Task1: check foooter items`, function () {
+                checkEyesWindow(elements.mainFooter());
+            });
+
+
+        });
+
+        // describe.skip('color changing elements on hover on viewport', () => {
+        //  it('shoul show elements that change color on hover', () => {
+        //   cy.get(elements.wishListIconNavbar()).invoke('show').trigger('mouseenter')
+        //   .should('have.css', 'color', '#004dda')
+        //  })
+        // });
+
+
+    });
+
+    describe('Task 1: Desktop Viewport Tests', () => {
+        beforeEach(() => {
+            cy.eyesOpen({
+                browser: [
+                    ...desktopViewports
+                ],
+            });
+        })
+        afterEach(() => {
+            cy.eyesClose();
+        });
+        it('Task 1: Shoes Filter Section', () => {
+            checkEyesWindow(elements.shoesFilterSection());
+        });
+
+    });
+
+    describe('Task 1: Tablet Viewport Tests', () => {
+        beforeEach(() => {
+            cy.viewport(768, 700);
+            cy.eyesOpen({
+                browser: [
+                    ...tabletViewports
+                ],
+            });
+        })
+        afterEach(() => {
+            cy.eyesClose();
+        });
+        it('Task 1: Shoes Filter Section', () => {
+            actions.openFilterNavigationSideBar();
+            checkEyesWindow(elements.shoesFilterSection());
+        });
+    });
+
+    describe('Task 1 Mobile Viewport Tests', () => {
+        beforeEach(() => {
+            cy.eyesOpen({
+                browser: [
+                    ...iphoneViewport
+                ],
+            });
+            cy.viewport('iphone-x');
+        })
+        afterEach(() => {
+            cy.eyesClose();
+        });
+        it('Task 1: Shoes Filter Section', () => {
+            actions.openFilterNavigationSideBar();
+            checkEyesWindow(elements.shoesFilterSection());
+        });
+
+    });
+
+
 
 });
