d2354b83b4be83c0e7ad1e9b4f01c142ccaf0aad
Waweru <amby.mwaz@gmail.com>
2020-06-23 23:49:34 +0300

    fix: shopping experience modern tests

--- cypress/tests/ModernTests/shopping-experience.spec.js
+++ cypress/tests/ModernTests/shopping-experience.spec.js
@@ -4,101 +4,24 @@ import { actions, elements, expects } from '../../pages/pageObjects';
 import {
     checkEyesWindow,
 } from '../../utils/utilityFunctions';
-import {
-    iphoneViewport,
-    tabletViewports,
-    desktopViewports,
-} from '../../utils/viewports';
-
 
 context('Filter Results', () => {
 
     beforeEach(() => {
+        cy.viewport(800, 600);
         actions.goToV1AppUrl();
+        cy.eyesOpen();
     });
 
-    describe('Task2: Desktop Viewport Tests', () => {
-        beforeEach(() => {
-            cy.eyesOpen({
-                browser: [ ...desktopViewports ]
-            });
-        });
-
-        afterEach(() => {
-            cy.eyesClose();
-        });
-
-        it(`can filter all black shoes based on color`, function () {
-            expects.colorFilterIsVisible();
-            actions.clickBlackColorFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-        it(`can filter the first black shoe based on the price`, function () {
-            expects.priceFilterIsVisible();
-            actions.clickZeroToFiftyPriceFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-
+    afterEach(() => {
+        cy.eyesClose();
     });
 
-    describe('Task2: Tablet Viewport Tests', () => {
-        beforeEach(() => {
-            cy.viewport(768, 700);
-            cy.eyesOpen({
-                browser: [...tabletViewports]
-            });
-        });
-
-        afterEach(() => {
-            cy.eyesClose();
-        });
-
-        it(`can filter all black shoes based on color`, function () {
-            actions.openFilterNavigationSideBar();
-            expects.colorFilterIsVisible();
-            actions.clickBlackColorFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-        it(`can filter the first black shoe based on the price`, function () {
-            actions.openFilterNavigationSideBar();
-            expects.priceFilterIsVisible();
-            actions.clickZeroToFiftyPriceFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-
+    it(`Task 2: can filter all black shoes based on color`, function () {
+        actions.openFilterNavigationSideBar();
+        expects.colorFilterIsVisible();
+        actions.clickBlackColorFilter();
+        actions.clickFilterButton();
+        checkEyesWindow(elements.productGrid());
     });
-
-    describe('Task2: Mobile Viewport Tests', () => {
-        beforeEach(() => {
-            cy.viewport('iphone-x');
-            cy.eyesOpen({
-                browser: [...iphoneViewport]
-            });
-        });
-
-        afterEach(() => {
-            cy.eyesClose();
-        });
-
-        it(`can filter all black shoes based on color`, function () {
-            actions.openFilterNavigationSideBar();
-            expects.colorFilterIsVisible();
-            actions.clickBlackColorFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-        it(`can filter the first black shoe based on the price`, function () {
-            actions.openFilterNavigationSideBar();
-            expects.priceFilterIsVisible();
-            actions.clickZeroToFiftyPriceFilter();
-            actions.clickFilterButton();
-            checkEyesWindow(elements.gridItems());
-        });
-
-    });
-
 });
