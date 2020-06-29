92f9c178bd373ccb3e82533062790ff9b85305fa
Waweru <amby.mwaz@gmail.com>
2020-06-30 00:40:51 +0300

    fix: modify ru commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.on('fail', (error, runnable) => {
+    // debugger
+    throw error 
+  })
+  
