b1162a69d68e570756eadfca01206fafd2202cdb
Waweru <amby.mwaz@gmail.com>
2020-07-04 01:47:16 +0300

    fix: modern tests separation

---
+++ cypress/tests/ModernTestsV1/shopping-experience.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="@applitools/eyes-cypress" />
+
+import { actions, elements, expects } from '../../pagesV1/pageObjects';
+import {
+    checkEyesWindow,
+} from '../../utils/utilityFunctions';
+
+context('Filter Results', () => {
+
+    beforeEach(() => {
+        cy.viewport(800, 600);
+        actions.goToV2AppUrl();
+        cy.eyesOpen();
+    });
+
+    afterEach(() => {
+        cy.eyesClose();
+    });
+
+    it(`Task 2: can filter all black shoes based on color`, function () {
+        actions.openFilterNavigationSideBar();
+        expects.colorFilterIsVisible();
+        actions.clickBlackColorFilter();
+        actions.clickFilterButton();
+        checkEyesWindow(elements.productGrid());
+    });
+});
