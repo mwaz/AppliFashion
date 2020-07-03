0843f29ca48ffc333bd57c45143e7bd9c59f70e4
Waweru <amby.mwaz@gmail.com>
2020-07-04 01:47:28 +0300

    fix: modern tests separation by versions

---
+++ cypress/tests/ModernTestsV2/cross-device-tests.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="@applitools/eyes-cypress" />
+
+import { actions } from '../../pagesV1/pageObjects';
+
+context('Cross-Device Elements Tests', () => {
+
+    beforeEach(() => {
+        actions.goToV2AppUrl();
+    });
+
+    beforeEach(() => {
+        cy.viewport(800, 600);
+        cy.eyesOpen();
+    });
+
+    afterEach(() => {
+        cy.eyesClose();
+    });
+    it(`Task 1`, function () {
+        cy.eyesCheckWindow('Main Page Sections')
+    });
+});
---
+++ cypress/tests/ModernTestsV2/product-details.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="@applitools/eyes-cypress" />
+
+import { actions, expects } from '../../pagesV1/pageObjects';
+
+
+context('Product Details test', () => {
+
+    beforeEach(() => {
+        cy.viewport(800, 600);
+        actions.goToV2AppUrl();
+        cy.eyesOpen();
+    });
+
+    afterEach(() => {
+        cy.eyesClose();
+    });
+
+    it(`Task 3`, function () {
+        actions.clickFirstBlackShoe();
+        expects.validateProductImageUrl({ productId: 1 });
+        cy.eyesCheckWindow('First Shoe Product Page')
+    });
+});
