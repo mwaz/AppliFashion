da49c14cbb22e6caf45b2c16383895eb308082c5
Waweru <amby.mwaz@gmail.com>
2020-07-05 22:06:50 +0300

    fix: update v1 shopping experience tests

--- cypress/tests/TraditionalTestsV1/shopping-experience.spec.js
+++ cypress/tests/TraditionalTestsV1/shopping-experience.spec.js
@@ -5,43 +5,49 @@ import {
     checkSizes,
     switchViewports,
     fetchTestTitle
-  } from '../../utils/utilityFunctions';
-  import { actions, elements, expects } from '../../pagesV1/pageObjects';
-  
-  const viewports = {
+} from '../../utils/utilityFunctions';
+import { actions, elements, expects } from '../../pagesV1/pageObjects';
+
+const viewports = {
     laptop: [1200, 700],
     tablet: [768, 700],
     mobile: [375, 812],
-  }; 
-  const { laptop, tablet, mobile } = viewports;
-  const testTask = 2;
-  
-  context('Product Experience', () => {
+};
+const { laptop, tablet, mobile } = viewports;
+const testTask = 2;
+
+context('Product Experience', () => {
     beforeEach(() => {
         actions.goToV1AppUrl();
     });
-    afterEach(() => {
-        actions.resetFilter();
-    });
-  
+
+    afterEach(function () {
+        if (this.currentTest.state === 'failed') {
+            const viewport = `${this.currentTest.commands[2].viewportWidth} X ${this.currentTest.commands[2].viewportHeight}`
+            cy.writeFile(`Traditional-V2-TestResults.txt`, `"Task: 1, Test Name: ${this.currentTest.title}, DOM Id: ${this.currentTest.commands[2].message}, Browser: ${Cypress.env('browser')}, Viewport: ${viewport}, Device: ${checkDevice(viewport)}, Status: ${this.currentTest.state}\n`, { flag: "a+" })
+        }
+    })
+
     describe('Desktop Tests', () => {
-  
+        afterEach(() => {
+            actions.resetFilter();
+        });
         beforeEach(() => {
             cy.viewport(laptop[0], laptop[1])
         });
         describe('Filter for black shoes on viewport (1200 X 700)', () => {
-            it('can filter all black shoes based on color', function(){
+            it('can filter all black shoes based on color', function () {
                 expects.colorFilterIsVisible();
                 actions.clickBlackColorFilter();
                 actions.clickFilterButton();
                 shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(laptop), 2, testTask);
             });
-            it('can filter the first black shoe based on the price', function(){
+            it('can filter the first black shoe based on the price', function () {
                 expects.colorFilterIsVisible();
                 actions.clickZeroToFiftyPriceFilter();
                 actions.clickFilterButton();
                 shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(laptop), 1, testTask);
-  
+
             });
         });
     });
@@ -52,24 +58,23 @@ import {
                 actions.resetFilter();
             })
             describe(`Filter for black shoes [${checkSizes(size)}]`, () => {
-                it(`can filter all black shoes based on color`, function(){
-                  switchViewports(size)
+                it(`can filter all black shoes based on color`, function () {
+                    switchViewports(size)
                     actions.openFilterNavigationSideBar();
                     expects.colorFilterIsVisible();
                     actions.clickBlackColorFilter();
                     actions.clickFilterButton();
                     shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(size), 2, testTask);
                 });
-                it(`can filter the first black shoe based on the price`, function(){
-                  switchViewports(size)
+                it(`can filter the first black shoe based on the price`, function () {
+                    switchViewports(size)
                     actions.openFilterNavigationSideBar();
                     expects.priceFilterIsVisible();
                     actions.clickZeroToFiftyPriceFilter();
                     actions.clickFilterButton();
                     shouldEqual(fetchTestTitle(this), elements.gridItems(), viewPortSize(size), 1, testTask);
-  
                 });
             })
         });
     });
-  });
+});
