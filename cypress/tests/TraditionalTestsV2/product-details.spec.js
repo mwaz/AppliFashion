aa5fb0e172f91f829a08b6afe59a7d6cbcf54ba3
Waweru <amby.mwaz@gmail.com>
2020-07-05 22:07:45 +0300

    fix: update v2 product details tests

--- cypress/tests/TraditionalTestsV2/product-details.spec.js
+++ cypress/tests/TraditionalTestsV2/product-details.spec.js
@@ -4,9 +4,10 @@ import {
   checkSizes,
   switchViewports,
   fetchTestTitle,
-  shouldBeVisible
+  shouldBeVisible,
+  checkDevice
 } from '../../utils/utilityFunctions';
-import { actions, elements, expects } from '../../pages/pageObjects';
+import { actions, elements, expects } from '../../pagesV2/pageObjects';
 
 const viewports = {
   laptop: [1200, 700],
@@ -18,22 +19,43 @@ const testTask = 3;
 
 context('Product Experience', () => {
   beforeEach(() => {
-      actions.goToV1AppUrl();
+      actions.goToV2AppUrl();
   });
+
+  afterEach(function(){
+    if (this.currentTest.state === 'failed') {
+      const viewport = `${this.currentTest.commands[2].viewportWidth} X ${this.currentTest.commands[2].viewportHeight}`
+        cy.writeFile(`Traditional-V2-TestResults.txt`, `"Task: ${testTask}, Test Name: ${this.currentTest.title}, DOM Id: ${this.currentTest.commands[2].message}, Browser: ${Cypress.env('browser')}, Viewport: ${viewport}, Device: ${checkDevice(viewport)}, Status: ${this.currentTest.state}\n`, { flag: "a+" })
+      }
+    })
 
   describe('All Viewport tests', () => {
       const viewports = [tablet, mobile, laptop];
       viewports.forEach((size) => {
           
           describe(`Naviagate to product page [${checkSizes(size)}]`, () => {
-              it(`can click the first shoe image and validate details`, function(){
+              it(`can click the first shoe image and verify product and url details`, function(){
                 switchViewports(size)
                   actions.clickFirstBlackShoe();
-                  expects.validateProductImageElements()
+                  expects.validateProductImageElements();
+                  expects.validateCssProperies(elements.productPageShoeOldPrice(), 'text-decoration', 'line-through solid rgb(153, 153, 153)');
                   expects.validateProductImageUrl({productId : 1});
                   shouldBeVisible(fetchTestTitle(this), elements.productPageShoeImage(), viewPortSize(size), testTask);
+              });
+              it(`can verify all shoe dropdown sizes text`, function(){
+                switchViewports(size)
+                actions.clickFirstBlackShoe();
+                actions.clickCurrentShoeSelection();  
+                expects.validateAllShoeSizes();
+                shouldBeVisible(fetchTestTitle(this), elements.shoeSizeDropdownElements(1), viewPortSize(size), testTask);
+              });
+              it(`can verify product page rating text is Properly displayed`, function(){
+                switchViewports(size)
+                actions.clickFirstBlackShoe();
+                expects.validateRatingCountTextDisplay();
+                shouldBeVisible(fetchTestTitle(this), elements.ratingCountText(), viewPortSize(size), testTask);
               });
           })
       });
   });
-});
+})
